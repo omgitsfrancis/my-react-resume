0a5aab68cd8e47180844dc410c12a571dbe48b55
omgitsfrancis <omgitsfrancis@gmail.com>
2020-03-16 18:50:53 -0700

    Refactored header section to TS

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,35 +1,39 @@
-import React from 'react';
-import styled from '@react-pdf/styled-components';
-import { headerData } from '../data/basic'
+import React from "react";
+import styled from "@react-pdf/styled-components";
 
 const Wrapper = styled.View`
   display: flex;
   flex-direction: column;
-`
+`;
 const Name = styled.Text`
   font-size: 36px;
   text-align: center;
   font-weight: bold;
   line-height: 1.2;
-`
+`;
 const Title = styled.Text`
   font-size: 10px;
   font-weight: light;
   text-align: center;
   line-height: 1.5;
   color: ${props => props.theme.accent};
-`
-const Contact =styled.Text`
+`;
+const Contact = styled.Text`
   font-size: 10px;
   font-weight: light;
   text-align: center;
-`
-export default function Header({name, title, contacts}) {
+`;
+export default function Header(props: {
+  name: string;
+  title: string;
+  contacts: string[];
+}) {
+  const { name, title, contacts } = props;
   return (
     <Wrapper>
       <Name>{name}</Name>
       <Title>{title}</Title>
-      <Contact>{contacts.join('   |   ')}</Contact>      
+      <Contact>{contacts.join("   |   ")}</Contact>
     </Wrapper>
-  )
+  );
 }
