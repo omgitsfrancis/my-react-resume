bc694ed24c506162209c8faca72d368d09b671b0
omgitsfrancis <omgitsfrancis@gmail.com>
2020-04-13 17:24:59 -0700

    Updated projects data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,19 +2,19 @@ import { ProjectData } from "../sections/Projects";
 
 export const projectsData: ProjectData[] = [
   {
+    projectName: "Web Scraper",
+    bullets: [
+      "Given a URL and CSS Selector, this application will scrape a web page and return text data that matches the CSS Selector.",
+      "Client is hosted on Netlify and public API is hosted on Heroku.",
+      "Created with TypeScript, React, and Node."
+    ],
+  },
+  {
     projectName: "Lululemon Stock Checker",
     bullets: [
       "Created an Express server that would email me when a specific Lululemon product was available online.",
       "Modules used: axios for requests, cheerio for parsing HTML, and nodemailer to send emails.",
-      "Deployed the server to the cloud using Azure App Service."
-    ]
+      "Deployed the server to the cloud using Azure App Service.",
+    ],
   },
-  {
-    projectName: "React Resume",
-    bullets: [
-      "Took my resume template and turned it into a React application.",
-      "Resume can be deployed as a static website or be exported to PDF.",
-      "Application has been converted to Typescript."
-    ]
-  }
 ];
