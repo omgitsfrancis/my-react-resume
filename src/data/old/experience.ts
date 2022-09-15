62ac76b8a5d5a400835acc03471288351860d965
omgitsfrancis <omgitsfrancis@gmail.com>
2022-09-14 23:07:16 -0700

    keep old

---
+++ src/data/old/experience.ts
@@ -0,0 +1,85 @@
+import { ExperienceData } from "../../sections/Experience";
+
+export const experienceData: ExperienceData[] = [
+  {
+    company: "InnovaSystems International",
+    location: "San Diego, CA",
+    positions: [
+      {
+        title: "Software Engineer",
+        started: "Dec 2019",
+        ended: "Jul 2021",
+      },
+      {
+        title: "Software QA Engineer",
+        started: "Nov 2018",
+        ended: "Dec 2019",
+      },
+    ],
+    bullets: [
+      "As the sole QA team member, I created and managed test plans and test cases for manual test execution.",
+      "Conducted Application Integration and Software Integration Testing (AI SIT) to validate web application functionality on CANES.",
+      "Developed automated end to end regression tests with Selenium and TestCafe.",
+      "Contributed to the company's design system by creating styled React components.",
+    ],
+  },
+  {
+    company: "Forward Slope Inc.",
+    location: "San Diego, CA",
+    positions: [
+      {
+        title: "Junior Software Engineer",
+        started: "Aug 2017",
+        ended: "Nov 2018",
+      },
+    ],
+    bullets: [
+      "Developed automated security patching scripts for isolated NITES IV systems and all its variants.",
+      "Utilized security assessment tools such as Nessus, Security Content Automation Protocol (SCAP), and Security Technical Implementation Guides (STIGs) to perform vulnerability testing.",
+      "Conducted Application Integration and Software Integration Testing (AI SIT) of NITES TDAs (Tactical Decision Aids) on various government networks(CANES, RDTE, SWAN).",
+      "Familiar with the Certification and Accreditation Process (C&A) by performing security evaluations on NITES IV systems.",
+    ],
+  },
+  {
+    company: "Koam Engineer Systems - KES inc",
+    location: "San Diego, CA",
+    positions: [
+      {
+        title: "Computer Systems Engineer",
+        started: "Oct 2016",
+        ended: "Aug 2017",
+      },
+    ],
+    bullets: [
+      "Developed automation using Government proprietary software accompanied with Python and PowerShell scripts.",
+      "Executed and debugged automated functional tests on Naval Ship systems such as CANES.",
+      "Created thorough tutorial documentation to provide training on the creation and execution of automated test.",
+      "Managed all test assets by creating baseline images, backups, and documentation.",
+      "Wrote daily testing reports to update installation technicians of system discrepancies and the testing schedule.",
+    ],
+  },
+  {
+    company: "Tapestry Solutions, a Boeing Company",
+    location: "San Diego, CA",
+    positions: [
+      {
+        title: "IT Technician",
+        started: "Mar 2016",
+        ended: "Oct 2016",
+      },
+      {
+        title: "System Administrator Intern",
+        started: "Jul 2015",
+        ended: "Mar 2016",
+      },
+    ],
+    bullets: [
+      "Support and troubleshoot technological issues of a company with over 500 employees.",
+      "Experience with Enterprise Microsoft technologies such as Active Directory, SCCM, Forefront Identity Manager, IIS, and Exchange.",
+      "Managed and deployed VMs with using hypervisors such as VMware's Vsphere and Microsoft's Hyper-V.",
+      "Issued laptops, desktops, Mitel phones, and smart cards to employees of San Diego location",
+      "Monitor and support user help tickets through the Spiceworks ticketing system.",
+      "Projects included Classroom in a box, Facility Maintenance Support Ticket Server, and HTTPS file server for customer downloads.",
+    ],
+  },
+];
